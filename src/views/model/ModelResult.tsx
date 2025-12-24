7f016672b93463eb30ba4f286e88c10b61d7507e
cham <cham@LAPTOP-64B3OTV6.>
2025-12-24 18:23:20 +0800

    feat: add a fallback to invalid json data of ai result

--- src/views/model/ModelResult.tsx
+++ src/views/model/ModelResult.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState, type JSX } from "react";
 import PlantReport from "./PlantReport";
+import { AlertCircle } from "lucide-react";
 
 interface Props {
   text: string;
@@ -9,7 +10,12 @@ export default function ModelResult(props: Props) {
   const [text, setText] = useState<JSX.Element>(<span>{props.text}</span>);
   useEffect(() => {
     if (props.type === "error") {
-      const span = <span>{props.text}</span>;
+      const span = (
+        <div className="p-8 text-center text-red-500 bg-red-50 rounded-lg border border-red-200">
+          <AlertCircle className="w-10 h-10 mx-auto mb-2" />
+          <p>无法解析 json 数据，请检查输入图片是否清晰。</p>
+        </div>
+      );
       setText(span);
       return;
     }
